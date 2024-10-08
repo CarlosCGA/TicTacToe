f7b533d7f44c936247ff209e1bcd51a3bfba5f9e
Carlos <carloscga97@gmail.com>
2024-10-08 08:24:33 +0200

    Add firebase dependency injection

--- app/src/main/java/com/cazulabs/tictactoe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/cazulabs/tictactoe/ui/home/HomeViewModel.kt
@@ -1,11 +1,13 @@
 package com.cazulabs.tictactoe.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.cazulabs.tictactoe.data.network.FirebaseService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(private val firebaseService: FirebaseService) :
+    ViewModel() {
     fun onCreateGame() {
         TODO("Not yet implemented")
     }
