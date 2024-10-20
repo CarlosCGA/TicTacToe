462c4deda466426d30c424b8b50cc3f73e925b70
Carlos <carloscga97@gmail.com>
2024-10-20 02:37:17 +0200

    Add dialog to join game

--- app/src/main/java/com/cazulabs/tictactoe/ui/home/HomeScreen.kt
+++ app/src/main/java/com/cazulabs/tictactoe/ui/home/HomeScreen.kt
@@ -1,10 +1,7 @@
 package com.cazulabs.tictactoe.ui.home
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,45 +9,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Text
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.invisibleToUser
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.cazulabs.tictactoe.R
 import com.cazulabs.tictactoe.ui.core.ClashRoyaleButton
@@ -68,14 +41,32 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
     navigateToGame: (String, String, Boolean) -> Unit
 ) {
+    var showJoinGameDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
         Background()
-        Body(viewModel, navigateToGame)
+        Body(
+            viewModel,
+            navigateToGame,
+            onClickJoinGameButton = { showJoinGameDialog = true }
+        )
     }
+
+    if (showJoinGameDialog)
+        JoinGameDialog(
+            onDismiss = { showJoinGameDialog = false },
+            onJoinGame = { gameName -> viewModel.onJoinGame(gameName, navigateToGame) }
+        )
 }
 
 @Composable
-fun Body(viewModel: HomeViewModel, navigateToGame: (String, String, Boolean) -> Unit) {
+fun Body(
+    viewModel: HomeViewModel,
+    navigateToGame: (String, String, Boolean) -> Unit,
+    onClickJoinGameButton: () -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -92,23 +83,28 @@ fun Body(viewModel: HomeViewModel, navigateToGame: (String, String, Boolean) -> 
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            ClashRoyaleButton(
-                color3 = Orange3,
-                color2 = Orange2,
-                color1 = Orange1,
-                colorSpotShadow = Color.Red,
-                colorDetail = OrangeDetail,
-                text = "Battle",
-                onClick = { viewModel.onCreateGame(navigateToGame) }
-            )
+            CreateGameButton(onClick = { viewModel.onCreateGame(navigateToGame) })
 
             Spacer(modifier = Modifier.size(16.dp))
 
-            JoinTheGame(onJoinGame = { viewModel.onJoinGame(it, navigateToGame) })
+            JoinTheGame(onClick = onClickJoinGameButton)
         }
 
         Spacer(modifier = Modifier.weight(1F))
     }
+}
+
+@Composable
+fun CreateGameButton(onClick: () -> Unit) {
+    ClashRoyaleButton(
+        color3 = Orange3,
+        color2 = Orange2,
+        color1 = Orange1,
+        colorSpotShadow = Color.Red,
+        colorDetail = OrangeDetail,
+        text = "Battle",
+        onClick = onClick
+    )
 }
 
 @Composable
@@ -122,19 +118,46 @@ fun Background() {
 }
 
 @Composable
-fun JoinTheGame(onJoinGame: (String) -> Unit) {
-    var gameName by rememberSaveable {
-        mutableStateOf("")
-    }
-
+fun JoinTheGame(onClick: () -> Unit) {
     ClashRoyaleButton(
         color3 = Blue3,
         color2 = Blue2,
         color1 = Blue1,
         colorDetail = BlueDetail,
         text = "Join",
-        onClick = {
-            onJoinGame(gameName)
-        }
+        onClick = onClick
     )
+}
+
+@Composable
+fun JoinGameDialog(onDismiss: () -> Unit, onJoinGame: (String) -> Unit) {
+    var gameName by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Dialog(onDismissRequest = onDismiss) {
+        Box(modifier = Modifier.background(Color.White, RoundedCornerShape(6.dp))) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.size(16.dp))
+
+                OutlinedTextField(value = gameName, onValueChange = { gameName = it })
+                Spacer(modifier = Modifier.size(16.dp))
+                ClashRoyaleButton(
+                    color3 = Blue3,
+                    color2 = Blue2,
+                    color1 = Blue1,
+                    colorDetail = BlueDetail,
+                    text = "OK"
+                ) {
+                    onJoinGame(gameName)
+                    onDismiss()
+                }
+
+                Spacer(modifier = Modifier.size(16.dp))
+            }
+        }
+    }
 }
